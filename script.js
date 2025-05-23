1a62bc6d242721c12c333fa76cfde223600f934f
Joakimglodedata <joakim.glodedata@gmail.com>
2025-05-23 14:49:45 +0200

    "ferdig"

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // fishPond.html
-
 const fishContainer = document.querySelector("#fishContainer")
+console.log(fishContainer)
 
 const fish = document.createElement("p")
 fish.classList.add("fish")
 fish.textContent = ""
 console.log(fish.style.fontSize)
 
-// fishContainer.appendChild(fish)
-// Array.from({ length: 50 }, () => fishContainer.appendChild(fish.cloneNode(true)));
-
-const fishPossibilities = ["🐟", "🐠", "🐡", "🦈", "🐋", ]
-for (let i = 0; i < Math.floor((Math.random() * 100) + 20); i++) {
-   const x = Math.floor(Math.random() * 4)
-   fish.textContent = fishPossibilities[x];
-   fish.style.fontSize = Math.floor((Math.random() * 100) + 20)+"px";
-   fishContainer.appendChild(fish.cloneNode(true));
+const fishPossibilities = ["🐟", "🐠", "🐡", "🦈", "🐋", "🐙", "🦑", "🦞", "🦀", "🐬"]
+if (fishContainer != null) {
+    for (let i = 0; i < Math.floor((Math.random() * 100) + 20); i++) {
+        const x = Math.floor(Math.random() * 10)
+        fish.textContent = fishPossibilities[x];
+        fish.style.fontSize = Math.floor((Math.random() * 100) + 20)+"px";
+        fishContainer.appendChild(fish.cloneNode(true));
+     }
 }
